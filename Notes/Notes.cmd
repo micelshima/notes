bec09d19a0211a4986d007514dbbdeeb60e60543
Mikel V <micelshima@users.noreply.github.com>
2017-01-13 19:20:15 +0100

    Add files via upload

---
+++ Notes/Notes.cmd
@@ -0,0 +1,2 @@
+powershell -noprofile -windowstyle Hidden -file "%~dp0\data\Notes.ps1"
+
---
+++ Notes/data/css.ps1
@@ -0,0 +1,8 @@
+#hoja de estilos
+$css_textbox=""|select Bigfont,NormalFont,Smallfont
+$css_textbox.Bigfont=new-object System.Drawing.Font("Lucida Console",12)
+$css_textbox.Normalfont=new-object System.Drawing.Font("Lucida Console",10)
+$css_textbox.Smallfont=new-object System.Drawing.Font("Lucida Console",8)
+
+$css_buttonery=""|select font
+$css_buttonery.font=new-object System.Drawing.Font("Trebuchet MS",10,1,3,0)
